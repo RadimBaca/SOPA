66815f3ad74986ff58bcd7e1389a769f43394498
Radim Baca <bac027@vsb.cz>
2019-08-19 15:06:54 +0200

    Opravy v ocekavanem vystupu

--- ukol3/mustr.py
+++ ukol3/mustr.py
@@ -1,4 +1,4 @@
 
 # Ocekavany vystup spravneho reseni je
-# Rank 1 ma osoba s cislem 34 (rank je 17)
-# Rank 2 ma osoba s cislem 1 (rank je 16)
+# Rank 1 ma osoba s cislem 34 (stupen vrcholu je 17)
+# Rank 2 ma osoba s cislem 1 (stupen vrcholu je 16)
